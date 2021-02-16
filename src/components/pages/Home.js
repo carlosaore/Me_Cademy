65ea5a5cae54efa1e3067224124be104f1a82d37
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-02-16 22:28:27 +0100

    removed useless imports

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,32 +2,24 @@ import React from 'react';
 import MetaDecorator from '../metadecorator/MetaDecorator';
 import HomeSlider from '../molecules/HomeSlider';
 import HomeReg from '../molecules/HomeReg';
-import HomePartners from '../molecules/HomePartners';
 import HomeQuote from '../molecules/HomeQuote';
 import MainDiv from '../atoms/MainDivHome';
 import HomeHeroImage from '../molecules/HomeHeroImg';
-import HomeHeroImage2 from '../molecules/HomeHeroImage2';
 
 const Home = () => {
     return (
         <div>
-             <MetaDecorator
-             description='Home'
-             title='MeCademy'
-             />
+            <MetaDecorator
+                description='Home'
+                title='MeCademy'
+            />
             <HomeHeroImage />
-
             <MainDiv>
-            <HomeQuote />
-            <div className='home__form'>
-            <HomeSlider/>
-            <HomeReg/>
-            </div>
-            <HomeHeroImage2 />
-            <div className='home__logo'>
-            <HomePartners/>
-            </div>
-
+                <HomeQuote />
+                <div className='home__form'>
+                    <HomeSlider/>
+                    <HomeReg/>
+                </div>
             </MainDiv>
             
         </div>
