2ca39439374040bf41985fa0ab515b45f0d7d823
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2021-02-18 16:35:18 +0000

    removed row and fixed team comp

--- src/components/atoms/StyledTeamCompForm.js
+++ src/components/atoms/StyledTeamCompForm.js
@@ -4,71 +4,37 @@ const StyledTeamCompForm = styled.div `
 
 margin-bottom: 50px;
 
-.bottom-row {
+.row2 {
     display: flex;
-    justify-content: center;
-}
+    } 
 
-.row1 {
+.row3 {
     display: flex;
-    justify-content: center;
-    }
+    flex-direction: row-reverse;
+    }  
 
-.row2, .row3 {
-    display: flex;
-    }
-
-.img {
-    position: relative;
-    height: 400px;
-    object-fit: cover;
-    }
-
-.text {
-    position: relative;
-    height: 400px;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    justify-content: center;
-    padding: 0 30px 0 30px;
-    background-color: ${props => props.theme.colors.meCademyLightGrey};
-    color: ${props => props.theme.colors.meCademyTextGrey}
-    }   
-
-.text:after {
-    position: absolute;
-    left: -20px;
-    top: 180px;
-    content: '';
-    width: 0;
-    height: 0;
-    border-right: solid 20px #f5f5f5;
-    border-bottom: solid 20px transparent;
-    border-top: solid 20px transparent;
-    }
 
 .img2, .img3 {
+    display: flex;
     height: 250px;
     object-fit: cover;
     }
 
 .text2, .text3 {
+    display: flex;
     position: relative;
     height: 250px;
     background-color: ${props => props.theme.colors.meCademyLightTeal};
-    font-size: 12px;
     text-align: center;
-    display: flex;
     flex-direction: column;
     justify-content: center;
-    padding: 0 12px 0 12px;
+    padding: 0 30px 0 30px;
     color : ${props => props.theme.colors.meCademyTextGrey}
     }      
 
-.text2:after, .text3:after {
+.text2:after {
     position: absolute;
-    left: -15px;
+    left: -14px;
     top: 100px;
     content: '';
     width: 0;
@@ -76,65 +42,70 @@ margin-bottom: 50px;
     border-right: solid 15px ${props => props.theme.colors.meCademyLightTeal};
     border-bottom: solid 15px transparent;
     border-top: solid 15px transparent;
-    }
-@media screen and (max-width: 1290px){
-    .text2, .text3 {
-        font-size: 11px;
-    }
-    }
+    } 
 
-@media screen and (max-width: 1250px){
-
-    .row1 {
-        display: flex;
-        flex-wrap: wrap;
+.text3:after {
+    position: absolute;
+    right: -14px;
+    top: 100px;
+    content: '';
+    width: 0;
+    height: 0;
+    border-left: solid 15px ${props => props.theme.colors.meCademyLightTeal};
+    border-top: solid 15px transparent;
+    border-bottom: solid 15px transparent;
     }
+    
+    H1 {
+        margin: 3px;
+       }
+    
+    H3 {
+        margin: 7px;
+        }
 
-    .row2, .row3 {
-        display: flex; 
-    }
 
-    .bottom-row {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+   @media screen and (max-width: 1025px){
 
-    .text2, .text3 {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        font-size: 12px;
-    }
-
-    .text:after {
-            display: none;
+       H1 {
+        margin: 0;
+       }
+        H3 {
+        margin: 5px;
         }
    }
 
-   @media screen and (max-width: 651px){
-        .img {
-        width: 100%;
-        }
+    @media screen and (max-width: 865px){
+        font-size: 0.8em;
 
-        .text {
-        width: 100%;
-        padding: 0 30px 0 30px;
+       H1 {
+        margin: 0;
+       }
+        H3 {
+        margin: 5px;
         } 
+   }
 
-        .text:after, .text2:after, .text3:after {
+   @media screen and (max-width: 651px){
+    font-size: 0.87em;
+
+    H3 {
+        margin: 0;
+       }
+    
+
+    .text2:after, .text3:after {
         display: none;
         }
 
-        .row2, .row3 {
+    .row2, .row3 {
         display: flex;
         flex-direction: column; 
     }
-
-    .text2, .text3 {
-        font-size: 14px;
     }
-    }`
-
+   @media screen and (max-width: 395px){
+       font-size: 0.75em;
+    }
+    `
 
 export default StyledTeamCompForm;
