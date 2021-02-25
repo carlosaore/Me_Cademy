d30bbabea01563656a63c86b0d6f0c8617d2fd22
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-02-25 01:44:47 +0100

    replaced logo in this branch...

--- src/components/atoms/CarouselDiv.js
+++ src/components/atoms/CarouselDiv.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 
 const CarouselDiv = styled.div`
-    margin-bottom : 0;
     width : 100%;
+
+    @media (min-width : ${props => props.theme.largeViewport.size}) {
+        margin-bottom : ${props => props.theme.largeViewport.margin}
+    }
+
+    @media (min-width : ${props => props.theme.mediumViewport.minSize}) and (max-width : ${props => props.theme.mediumViewport.maxSize}) {
+        margin-bottom : ${props => props.theme.mediumViewport.margin}
+    }
+
+    @media (max-width : ${props => props.theme.smallViewport.size}) {
+        margin-bottom : ${props => props.theme.smallViewport.margin}
+    }
 `
 export default CarouselDiv;
