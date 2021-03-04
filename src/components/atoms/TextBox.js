d46b7681cbc61cbab045e2b5f2ef62557f6104fc
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2021-03-04 02:17:03 +0000

    resized some image, about numbered img line up

--- src/components/atoms/TextBox.js
+++ src/components/atoms/TextBox.js
@@ -1,6 +1,42 @@
 import styled from "styled-components";
 
 const TextBox = styled.article`
+
+
+.numbered {
+    max-width:40%;
+    margin:0 auto;
+    vertical-align:middle;
+    display: flex;
+    flex-direction: column;
+}
+
+img {
+    width: 30px;
+    margin-right: 5px;
+    vertical-align:middle;
+}
+
+@media screen and (max-width: 1265px) {
+    .numbered {
+    max-width:50%; 
+    }
+}
+
+@media screen and (max-width: 1000px) {
+    .numbered {
+    max-width:60%; 
+    }
+}
+
+@media screen and (max-width: 768px) {
+    .numbered {
+    max-width:70%; 
+    }
+}
+
+
+
 `;
 
 export default TextBox;
--- src/components/molecules/AboutArticles.js
+++ src/components/molecules/AboutArticles.js
@@ -9,14 +9,27 @@ export default function AboutArticles() {
                 <P>
                     Die Maßnahme der MentorMe gGmbH unterstützt weibliche arbeitssuchende & arbeitslose Akademikerinnen bei der zügigen Wiedereingliederung in den Arbeitsmarkt. Die Maßnahme umfasst ein Online-Programm und ein individuelles Coaching.</P>
                 <P justify alignCenter last>
-                    <b>Im 8-wöchigen Programm erhalten alle Teilnehmerinnen:</b>
-                </P>
-                <P justify alignCenter last>Beratung zu allen Schritten des Bewerbungsprozesses.</P>
-                <P justify alignCenter last>Persönliche Begleitung von erfahrenen TOP-Coaches.</P>
-                <P justify alignCenter last>Moderierter Austausch & Feedback in Online-Gruppen.</P>
-                <P justify alignCenter>Interaktive, praxisfokussierte, individualisierte Übungen.</P>
-                <P last>
-                    Das Ziel unseres Programmes ist, dich in den kommenden Wochen so stark wie möglich zu unterstützen und zu befähigen, deinen individuellen beruflichen Weg neu zu identifizieren, zu definieren und mit den professionellsten Tools auszustatten. Wir sind davon überzeugt, dass es immer Möglichkeiten gibt Veränderungen einzuleiten und besonders durch Kooperation, Netzwerk und Verständnis von Gleichgesinnten es leichter ist diese positiv zu erleben.
+                    <b>Im 8-wöchigen Programm erhalten alle Teilnehmerinnen:</b></P>
+                                        <br/>
+                                        <div className="numbered">
+                                                <P> <img className="one"
+                                                src="/images/one.png"
+                                                alt="one" />Beratung zu allen Schritten des Bewerbungsprozesses.</P>
+                                                
+                                                <P><img className="two"
+                                                src="/images/two.png"
+                                                alt="two" />Persönliche Begleitung von erfahrenen TOP-Coaches.</P>
+                                                
+                                                <P><img className="three"
+                                                src="/images/three.png"
+                                                alt="three" />Moderierter Austausch & Feedback in Online-Gruppen.</P>
+                                                
+                                                <P><img className="four"
+                                                src="/images/four.png"
+                                                alt="four" />Interaktive, praxisfokussierte, individualisierte Übungen.</P>
+                                            </div>
+                                            
+                <P last>Das Ziel unseres Programmes ist, dich in den kommenden Wochen so stark wie möglich zu unterstützen und zu befähigen, deinen individuellen beruflichen Weg neu zu identifizieren, zu definieren und mit den professionellsten Tools auszustatten. Wir sind davon überzeugt, dass es immer Möglichkeiten gibt Veränderungen einzuleiten und besonders durch Kooperation, Netzwerk und Verständnis von Gleichgesinnten es leichter ist diese positiv zu erleben.
                 </P>
             </TextBox>
         </ComponentWrapper>
